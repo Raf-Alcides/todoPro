1cfab0833e249159bc63a8ddb127e77fcb17b82f
Rafael da Silva Alcides <119860224+Raf-Alcides@users.noreply.github.com>
2023-05-24 19:16:37 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/Eudev12/todopro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.Eudev12.todopro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:todopro/app/app_module.dart';
+
+void main() {
+  runApp(const AppModule());
+}
+
+
